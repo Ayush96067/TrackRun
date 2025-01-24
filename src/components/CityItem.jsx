ebf6ffe745a173242f8ce13a58771e41480494e5
Ayush Sharma <ayushsharma722001@gmail.com>
2025-01-25 04:12:38 +0530

    adding useReducer in CityContext and performing add,delete functions.

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -15,7 +15,12 @@ const formatDate = (date) =>
 
 function CityItem({ city }) {
   const { cityName, emoji, date, id, position } = city;
-  const { currentCity } = useCities();
+  const { currentCity, deleteCity } = useCities();
+
+  function handleClick(e) {
+    e.preventDefault();
+    deleteCity(id);
+  }
 
   return (
     <li>
@@ -28,7 +33,9 @@ function CityItem({ city }) {
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button className={styles.deleteBtn}>&times;</button>
+        <button onClick={handleClick} className={styles.deleteBtn}>
+          &times;
+        </button>
       </Link>
     </li>
   );
